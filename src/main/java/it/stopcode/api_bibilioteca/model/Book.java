a07174cfd8fb1af78eef7b98ce65f67df7ee273a
LP Simeon <lp.simeon@yahoo.com>
2025-07-05 17:16:15 +0200

    Book class model added

---
+++ src/main/java/it/stopcode/api_bibilioteca/model/Book.java
@@ -0,0 +1,43 @@
+package it.stopcode.api_bibilioteca.model;
+
+import java.util.Date;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private Date publishYear;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(Date publishYear) {
+        this.publishYear = publishYear;
+    }
+}
