70936170b983320f086bd30641e66034942bbc21
LP Simeon <lp.simeon@yahoo.com>
2025-07-06 06:18:26 +0200

    getBookById method added in BookController

--- src/main/java/it/stopcode/api_bibilioteca/controller/BookController.java
+++ src/main/java/it/stopcode/api_bibilioteca/controller/BookController.java
@@ -4,6 +4,7 @@ import it.stopcode.api_bibilioteca.model.Book;
 import it.stopcode.api_bibilioteca.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,16 @@ public class BookController {
     @GetMapping
     public List<Book> getAllBooks() {
         return this.bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById (@PathVariable String id) {
+        Book book = this.bookService.getBookById(id);
+
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(book);
+        }
     }
 }
