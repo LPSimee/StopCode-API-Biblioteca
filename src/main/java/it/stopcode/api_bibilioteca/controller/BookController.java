a9606af95b70c447b43bf4ecde5ba516ebe92ae6
LP Simeon <lp.simeon@yahoo.com>
2025-07-06 06:12:48 +0200

    UserController added with GetMapping method getAllBooks()

---
+++ src/main/java/it/stopcode/api_bibilioteca/controller/BookController.java
@@ -0,0 +1,26 @@
+package it.stopcode.api_bibilioteca.controller;
+
+import it.stopcode.api_bibilioteca.model.Book;
+import it.stopcode.api_bibilioteca.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController (BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return this.bookService.getAllBooks();
+    }
+}
